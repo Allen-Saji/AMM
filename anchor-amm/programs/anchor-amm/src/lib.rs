37c9cf8583ad31fb7ce67b14e5b9d2d0f0007b22
Allen-Saji <allensaji04@gmail.com>
2024-12-22 20:47:39 +0530

    add instructions

--- anchor-amm/programs/anchor-amm/src/lib.rs
+++ anchor-amm/programs/anchor-amm/src/lib.rs
@@ -9,13 +9,31 @@ pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
-declare_id!("EGdJBLJzozGgDtZpnqowyuzEd3ioQYgjSCoW1Tgu7CBC");
+declare_id!("JCwLFmShANVSrGa8jgvUyMYeSyDxbYRLHBHdPnprKiTF");
 
 #[program]
 pub mod anchor_amm {
     use super::*;
-
     pub fn initialize(ctx: Context<InitializeConfig>, seed: u64, fee: u16) -> Result<()> {
         ctx.accounts.initialize_config(seed, fee, ctx.bumps)
+    }
+    pub fn swap(ctx: Context<Swap>, is_x: bool, amount: u64, min: u64) -> Result<()> {
+        ctx.accounts.swap(is_x, amount, min)
+    }
+    pub fn deposit(
+        ctx: Context<Deposit>, 
+        amount: u64, 
+        max_x: u64, 
+        max_y: u64
+    ) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y)
+    }
+
+    pub fn update(ctx: Context<UpdateConfig>, lock: bool) -> Result<()> {
+        if lock {
+            ctx.accounts.lock()
+        } else {
+            ctx.accounts.unlock()
+        }
     }
 }
